8422dfebc9b30d3e26e64655664996a306fca2ee
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2022-03-10 15:14:32 +0100

    Implemented A* in the new frogger_game file

--- frogger_game_old.py
+++ frogger_game_old.py
@@ -1,13 +1,12 @@
-# TODO sistemare check collisioni
-from pipes import stepkinds
+# Credit @vincenzosu
 
+import time
 import pygame
 import os
 import numpy as np
 import collections
-import time
 
-from frogger import FroggerProblem
+from frogger import FroggerProblem, FroggerState
 
 pygame.font.init()
 pygame.mixer.init()
@@ -25,11 +24,10 @@ FPS = 30
 class frogger_game:
     def __init__(self):
         self.state_matrix = np.asarray([
-
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
+            [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+            [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
             [1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0],
@@ -37,8 +35,6 @@ class frogger_game:
 
         ])
         self.frog_pos = (7, 7)
-        self.statemap = {}
-        self.path = None
 
     # 0-Nothing, 1-left, 2-up, 3-right, 4-down
     def step(self, action):
@@ -62,9 +58,10 @@ class frogger_game:
         r4 = list(r4)
         r5 = list(r5)
         r6 = list(r6)
-        self.state_matrix = np.asarray([self.state_matrix[0], r1, r2, r3, r4, r5, r6, self.state_matrix[7]])
+        self.state_matrix = np.asarray(
+            [self.state_matrix[0], r1, r2, r3, r4, r5, r6, self.state_matrix[7]])
 
-        if (action == 1):
+        if(action == 1):
             self.frog_pos = (self.frog_pos[0], max(0, self.frog_pos[1] - 2))
         elif (action == 2):
             self.frog_pos = (max(0, self.frog_pos[0] - 1), self.frog_pos[1])
@@ -73,71 +70,31 @@ class frogger_game:
         elif (action == 4):
             self.frog_pos = (min(7, self.frog_pos[0] + 1), self.frog_pos[1])
 
-        if (self.state_matrix[self.frog_pos[0]][self.frog_pos[1]] != 0):
+        if(self.state_matrix[self.frog_pos[0]][self.frog_pos[1]] != 0):
             lose = True
 
-        if (self.frog_pos[0] == 0):
+        if(self.frog_pos[0] == 0):
             win = True
         # print(action)
         return lose, win
 
-    def simple_reactive(self):
-        action = 0
-        if (self.frog_pos[0] >= 4 and self.frog_pos[0] <= 5):
-            if (self.state_matrix[self.frog_pos[0] - 1][self.frog_pos[1] + 1] == 0):
-                action = 2
-        else:
-            if (self.state_matrix[self.frog_pos[0] - 1][self.frog_pos[1] - 1] == 0):
-                action = 2
-
-        return action
-
-    def h(self, s): #H
-        distanceFromEndLane = s.y-1
-        return distanceFromEndLane
-
-    def a_star_path_to_actions(self):
-        if not self.path:
-            self.path = self.A_star_agent()
-
-        next_pose = self.path.pop(0)
-        return next_pose.move.value
-
-    def A_star_agent(self):
-        # fill this for the A* agent
-        # TODO Leggila dall'oggetto game
-        traffic_directions = [0, 1, 1, -1, -1, 1, 1, 0]
-
-        problem = FroggerProblem(self.state_matrix, traffic_directions, heuristic=self.h)
-        solution = problem.astar(show=True)
-
-        if solution == None:
-            print("   There is no solution")
-            return None
-
-        print(f"Best solution in {len(solution)} moves:")
-        s = problem.initial_state
-        for i, a in enumerate(solution):
-            print(f"{i+1:3}) {a}")
-            s = a.apply(s)
-            print(f"{'':<5} to {s}")
-
-        return solution
 
 def draw_window(game):
+
     WIN.fill((0, 0, 0))
 
     for r in range(len(game.state_matrix)):
         for c in range(len(game.state_matrix[r])):
-            if (game.state_matrix[r][c] == 1):
-                pygame.draw.rect(surface=WIN, color=(155, 155, 155), rect=(c * 40, r * 40, 40, 40), border_radius=3)
-    pygame.draw.circle(surface=WIN, color=(0, 255, 0),
-                       center=((game.frog_pos[1] * 40) + 16, (game.frog_pos[0] * 40) + 16), radius=16)
+            if(game.state_matrix[r][c] == 1):
+                pygame.draw.rect(surface=WIN, color=(155, 155, 155), rect=(
+                    c * 40, r * 40, 40, 40), border_radius=3)
+    pygame.draw.circle(surface=WIN, color=(0, 255, 0), center=(
+        (game.frog_pos[1] * 40) + 16, (game.frog_pos[0] * 40) + 16), radius=16)
     pygame.display.update()
 
 
 def draw_win():
-    # WIN.fill((0, 0, 0))
+    #WIN.fill((0, 0, 0))
     WIN.blit(win_surface, (220, 140))
     pygame.display.update()
     # Fix per non farlo crashare su Windows
@@ -146,12 +103,34 @@ def draw_win():
 
 
 def draw_lost():
-    # WIN.fill((0, 0, 0))
+    #WIN.fill((0, 0, 0))
     WIN.blit(lose_surface, (220, 140))
     pygame.display.update()
     # Fix per non farlo crashare su Windows
     time.sleep(1)
     exit(0)
+
+
+def solve(game: frogger_game):
+    # TODO Leggila dall'oggetto game
+    traffic_directions = [0, 1, 1, -1, -1, 1, 1, 0]
+
+    problem = FroggerProblem(game.state_matrix, traffic_directions, heuristic=(lambda s: s.y - 1))
+    solution = problem.astar(show=True)
+
+    if solution == None:
+        print("   There is no solution")
+        return None
+
+    print(f"Best solution in {len(solution)} moves:")
+    s = problem.initial_state
+    for i, a in enumerate(solution):
+        print(f"{i+1:3}) {a}")
+        s = a.apply(s)
+        print(f"{'':<5} to {s}")
+
+    for a in solution:
+        yield a.move.value
 
 
 def main():
@@ -161,6 +140,11 @@ def main():
     rate = 0
     lost = False
     win = False
+
+    actions = solve(game)
+
+    # t = 0
+    # print(f"Starting at ({game.frog_pos[1]}, {game.frog_pos[0]}, {t})")
     while run:
         clock.tick(FPS)
         if ((not lost) and (not win)):
@@ -175,27 +159,19 @@ def main():
 
         if ((not lost) and (not win)):
             action = 0
-#            action = game.simple_reactive()
-            action = game.a_star_path_to_actions()
-            '''
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if (event.key == pygame.K_LEFT):
-                        action = 1
-                    elif (event.key == pygame.K_UP):
-                        action = 2
-                    elif (event.key == pygame.K_RIGHT):
-                        action = 3
-                    elif (event.key == pygame.K_DOWN):
-                        action = 4
-                    elif (event.key == pygame.K_w):
-                        action = 0
-            '''
-            lost, win = game.step(action)
-            time.sleep(1)
 
+            try:
+                action = actions.send(None)
+            except StopIteration:
+                print("Cannot continue. There are no moves")
+                exit(1)
+            lost, win = game.step(action)
+            # t = (t + 1) % len(game.state_matrix[0])
+            # print(f"Arrived at ({game.frog_pos[1]}, {game.frog_pos[0]}, {t})")
+            time.sleep(0.5)
 
 main()
